3a92fe1aa8a4cc00c52ebb8d24193d3da4c20694
PplaysMine <pwiegand2001@gmail.com>
2022-05-06 13:25:33 +0100

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,354 @@
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          User:
+ *              type: object
+ *              required:
+ *                  - user
+ *                  - pass
+ *              properties:
+ *                  user:
+ *                      type: string
+ *                      description: Name of the user
+ *                  pass:
+ *                      type: string
+ *                      description: Password of the user
+ *              example:
+ *                  user: TestUser
+ *                  pass: TestPass
+ *          Token:
+ *              type: object
+ *              required:
+ *                  - token
+ *              properties:
+ *                  token: 
+ *                      type: string
+ *                      description: Bearer token
+ *          NewPass:
+ *              type: object
+ *              required:
+ *                  - newPass
+ *              properties:
+ *                  newPass:
+ *                      type: string
+ *                      description: Password that should replace the old one
+ *      securitySchemes:
+ *          bearerAuth:
+ *              type: http
+ *              scheme: bearer
+ *              bearerFormat: JWT
+ */
+
+ /**
+ * @swagger
+ *  tags:
+ *      name: User
+ *      description: API for user related operations.
+ */
+const config = require('../config');
+const express = require('express');
+const router = express.Router();
+const sql = require('mysql');
+const jwt = require('jsonwebtoken');
+
+function createSQLConnection() {
+    return sql.createConnection({
+        host: config.db.host,
+        user: config.db.user,
+        password: config.db.pass,
+        database: config.db.dbName,
+    });
+}
+
+const tokenSecret = config.server.tokenSecret
+
+function destroySQLConnectionOnError(con, res) {
+    res.sendStatus(500);
+    con.destroy();
+}
+
+function verifyToken(req, res, next) {
+    const header = req.headers['authorization'];
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+/**
+ * @swagger
+ *  paths:
+ *      /user/:
+ *          post:
+ *              summary: Test connection
+ *              tags: [User]
+ *              requestBody:
+ *                  required: false
+ *              responses:
+ *                  "403":
+ *                      description: Only for testing purposes, returns 403
+ */
+router.post('/', (req, res) => {
+    res.sendStatus(403);
+});
+
+/**
+ * @swagger
+ *  paths:
+ *      /user/login/:
+ *          get:
+ *              summary: Logs in a registered user
+ *              tags: [User]
+ *              requestBody:
+ *                  required: true
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/User'
+ *              responses:
+ *                  "200":
+ *                      description: User exists & is logged in
+ *                      content:
+ *                          application/json:
+ *                              schema:
+ *                                  $ref: '#/components/schemas/Token'
+ *                  "400":
+ *                      description: Missing / malformed request body
+ *                  "401":
+ *                      description: No user exists with this username
+ *                  "500":
+ *                      description: Internal server error
+ */
+router.get('/login', (req, res) => {
+    var b = req.body;
+
+    if(!b) {
+        res.sendStatus(400);
+        return;
+    }
+
+    const user = {
+        userName: b.user,
+        userPass: b.pass
+    }
+
+    jwt.sign({user}, tokenSecret, { expiresIn: "1hr" }, (err, token) => {
+        if(err) throw err;
+        if(b.user && b.pass) {
+            var con = createSQLConnection();
+            con.connect((err) => {
+                if(err) { res.sendStatus(500); throw err; }
+                else {
+                    con.query('SELECT * FROM user WHERE username=? AND password=?', [req.body.user, req.body.pass], (error, result, fields) => {
+                        if(error) destroySQLConnectionOnError(con, res);
+                        else {
+                            if(result.length === 0) res.sendStatus(401);
+                            else res.status(200).json({"token": token});
+                        }
+                        con.destroy();
+                    });
+                }
+            });
+        } else {
+            res.sendStatus(400);
+        }
+    });
+
+});
+
+/**
+ * @swagger
+ *  paths:
+ *      /user/register/:
+ *          post:
+ *              summary: Registers a user with given username & password
+ *              tags: [User]
+ *              requestBody:
+ *                  required: true
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/User'
+ *              responses:
+ *                  "201":
+ *                      description: User is successfully created
+ *                  "400":
+ *                      description: Missing / malformed request body
+ *                  "403":
+ *                      description: This username already exists
+ *                  "500":
+ *                      description: Internal server error
+ */
+router.post('/register', (req, res) => {
+    var b = req.body;
+
+    if(b.user && b.pass) {
+        var con = createSQLConnection();
+        con.connect((err) => {
+            if(err) { res.sendStatus(500); throw err; }
+            else {
+                con.query('SELECT * FROM user WHERE username=?', [b.user, b.pass], (error, result, fields) => {
+                    if(error) destroySQLConnectionOnError(con, res);
+                    else {
+                        if(result.length === 0) {
+                            con.query('INSERT INTO user (username, password) VALUES (?, ?)', [b.user, b.pass], (e, r, f) => {
+                                if(e) destroySQLConnectionOnError(con, res);
+                                else {
+                                    res.sendStatus(201);
+                                    con.destroy();
+                                }
+                            });
+                        } else {
+                            res.status(403).send("Username taken.");
+                            con.destroy();
+                        }
+                    }
+                });
+            }
+        });
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+/**
+ * @swagger
+ *  paths:
+ *      /user/deleteAccount/:
+ *          post:
+ *              summary: Deletes user with given username (requires bearer token)
+ *              tags: [User]
+ *              security:
+ *                  - bearerAuth: []
+ *              requestBody:
+ *                  required: false
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/User'
+ *              responses:
+ *                  "200":
+ *                      description: User is successfully deleted
+ *                  "403":
+ *                      description: Token could not be verified
+ *                  "404":
+ *                      description: User with given username does not exist
+ *                  "500":
+ *                      description: Internal server error
+ */
+router.post('/deleteAccount', verifyToken, (req, res) => {
+    jwt.verify(req.token, tokenSecret, (err, authData) => {
+        if(err) res.sendStatus(403);
+        else {
+            var con = createSQLConnection();
+            con.connect((err) => {
+                if(err) { res.sendStatus(500); throw err; }
+                else {
+                    con.query('SELECT * FROM user WHERE username=? AND password=?', [authData.user.userName, authData.user.userPass], (error, result, fields) => {
+                        if(error) destroySQLConnectionOnError(con, res);
+                        if(result.length > 0) {
+                            con.query('DELETE FROM user WHERE username=?', [authData.user.userName], (e, r, f) => {
+                                if(e) destroySQLConnectionOnError(con, res);
+                                else res.sendStatus(200);
+                                con.destroy();
+                            });
+                        } else {
+                            res.status(404).send("User does not exist.");
+                            con.destroy();
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+/**
+ * @swagger
+ *  paths:
+ *      /user/changePassword/:
+ *          put:
+ *              summary: Changes password of given user to new given password (requires bearer token)
+ *              tags: [User]
+ *              security:
+ *                  - bearerAuth: []
+ *              requestBody:
+ *                  required: true
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/NewPass'
+ *              responses:
+ *                  "200":
+ *                      description: Password successfully changed, returns new token with updated password
+ *                      content:
+ *                          application/json:
+ *                              schema:
+ *                                  $ref: '#/components/schemas/Token'
+ *                  "400":
+ *                      description: Missing / malformed request body
+ *                  "403":
+ *                      description: Token could not be verified
+ *                  "404":
+ *                      description: User with given username does not exist
+ *                  "500":
+ *                      description: Internal server error
+ */
+router.put('/changePassword', verifyToken, (req, res) => {
+    jwt.verify(req.token, tokenSecret, (err, authData) => {
+        if(err) res.sendStatus(403);
+        else {
+            var b = req.body;
+            if(b.newPass) {
+                var con = createSQLConnection();
+                con.connect((err) => {
+                    if(err) { res.sendStatus(500); throw err; }
+                    else {
+                        con.query('SELECT * FROM user WHERE username=? AND password=?', [authData.user.userName, authData.user.userPass], (error, result, fields) => {
+                            if(error) destroySQLConnectionOnError(con, res);
+                            if(result.length > 0) {
+                                con.query('UPDATE user SET password=? WHERE username=?', [b.newPass, authData.user.userName], (e, r, f) => {
+                                    if(e) destroySQLConnectionOnError(con, res);
+                                    else {
+                                        jwt.sign({username: authData.user.username, userPass: b.newPass}, tokenSecret, { expiresIn: "1hr" }, (err, token) => {
+                                            if(err) throw err;
+                                            res.status(200).json({"token": token});
+                                        });
+                                        con.destroy();
+                                    }
+                                });
+                            } else {
+                                res.sendStatus(404);
+                                con.destroy();
+                            }
+                        });
+                    }
+                });
+            } else {
+                res.sendStatus(400);
+            }
+        }
+    });
+});
+
+/**
+ * @swagger
+ *  paths:
+ *      /user/*:
+ *          post:
+ *              summary: All other routes return 418
+ *              tags: [User]
+ *              responses:
+ *                  "418":
+ *                      description: 
+ */
+router.post('*', (req, res) => {
+    res.sendStatus(418);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,63 @@
+const config = require('./config');
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const basicAuth = require('express-basic-auth');
+
+const options = {
+    key: fs.readFileSync(config.ssl.keyLoc),
+    cert: fs.readFileSync(config.ssl.certLoc),
+}
+
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "SELab 22A13 endpoint documentation",
+            version: "0.1.0",
+            description: "Splitted in two endpoints: User & Data (TBD)",
+            license: {
+            name: "MIT",
+            url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+            name: "",
+            url: "",
+            email: "",
+            },
+        },
+        servers: [
+            {
+                url: config.server.url
+            },
+        ],
+    },
+    apis: ["./routes/user.js", "./routes/data.js"],
+};
+
+const specs = swaggerJsdoc(swaggerOptions);
+
+function basicAuthAuthorizer(user, pass) {
+    const userMatches = basicAuth.safeCompare(user, config.docs.user);
+    const passwordMatches = basicAuth.safeCompare(pass, config.docs.pass);
+
+    return userMatches & passwordMatches;
+}
+
+app = express();
+app.use(express.json());
+app.use('/user', require('./routes/user'));
+app.use('/data', require('./routes/data'));
+app.use("/api-docs", basicAuth({
+    authorizer: basicAuthAuthorizer,
+    challenge: true
+}), swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
+
+var httpsServer = https.createServer(options, app);
+httpsServer.listen(config.server.port, () => {
+    console.log(`Listening on port ${config.server.port}`);
+});
+
+app.listen(config.server.swaggerPort);
