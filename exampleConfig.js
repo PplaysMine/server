74ea910e71e6613b4647245ebc8d6d89a7ba5fe0
PplaysMine <pwiegand2001@gmail.com>
2022-05-06 13:25:33 +0100

    Initial commit

---
+++ exampleConfig.js
@@ -0,0 +1,19 @@
+var config = {};
+
+config.ssl = {};
+config.db = {};
+config.server = {};
+
+config.ssl.keyLoc = '';
+config.ssl.certLoc = '';
+
+config.db.host = '';
+config.db.user = '';
+config.db.pass = '';
+config.db.dbName = '';
+
+config.server.port = 1;
+config.server.swaggerPort = 1;
+config.server.tokenSecret = '';
+
+module.exports = config;
---
+++ routes/data.js
@@ -0,0 +1,109 @@
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Data:
+ *              type: object
+ *              required:
+ *                  - data
+ *              properties:
+ *                  data:
+ *                      type: object
+ *                      description: A single data object
+ *              example:
+ *                  data: {}
+ *          AllData:
+ *              type: object
+ *              required:
+ *                  - dataArray
+ *              properties:
+ *                  dataArray:
+ *                      type: array
+ *                      description: All data associated with the user
+ *              example:
+ *                  dataArray: [{}]
+ *      securitySchemes:
+ *          bearerAuth:
+ *              type: http
+ *              scheme: bearer
+ *              bearerFormat: JWT
+ */
+
+ /**
+ * @swagger
+ *  tags:
+ *      name: Data
+ *      description: API for data related operations
+ */
+const config = require('../config');
+const express = require('express');
+const router = express.Router();
+const sql = require('mysql');
+const jwt = require('jsonwebtoken');
+
+function createSQLConnection() {
+    return sql.createConnection({
+        host: config.db.host,
+        user: config.db.user,
+        password: config.db.pass,
+        database: config.db.dbName,
+    });
+}
+
+const tokenSecret = config.server.tokenSecret
+
+function destroySQLConnectionOnError(con, res) {
+    res.sendStatus(500);
+    con.destroy();
+}
+
+function verifyToken(req, res, next) {
+    const header = req.headers['authorization'];
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+/**
+ * @swagger
+ *  paths:
+ *      /data/all/:
+ *          get:
+ *              summary: Returns all data associated with the user (requires bearer token)
+ *              tags: [Data]
+ *              security:
+ *                  - bearerAuth: []
+ *              requestBody:
+ *                  required: false
+ *              responses:
+ *                  "200":
+ *                      description: User exists, send all data associated with user
+ *                      content:
+ *                          application/json:
+ *                              schema:
+ *                                  $ref: '#/components/schemas/AllData'
+ */
+router.get('/all', verifyToken, (req, res) => {
+    jwt.verify(req.token, tokenSecret, (err, authData) => {
+        if(err) res.sendStatus(403);
+        else {
+            var con = createSQLConnection();
+            con.connect((err) => {
+                if(err) destroySQLConnectionOnError();
+                else {
+                    // con.query('SELECT * FROM user', (error, result, fields) => {
+                        res.sendStatus(200);
+                        con.destroy();
+                    // });
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
