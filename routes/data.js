d7aab980303949059bbe3f7990b1fe77ac15bdf4
paulwiegand <pwiegand2001@gmail.com>
2022-05-12 14:55:45 +0200

    Add swagger docs

--- routes/data.js
+++ routes/data.js
@@ -87,6 +87,8 @@ function verifyToken(req, res, next) {
  *                          application/json:
  *                              schema:
  *                                  $ref: '#/components/schemas/AllData'
+ *                  "403":
+ *                      description: Token could not be verified
  */
 router.get('/all', verifyToken, (req, res) => {
     jwt.verify(req.token, tokenSecret, (err, authData) => {
@@ -106,6 +108,23 @@ router.get('/all', verifyToken, (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ *  paths:
+ *      /data/setData/:
+ *          put:
+ *              summary: Add data for user (requires bearer token) 
+ *              tags: [data]
+ *              security:
+ *                  - bearerAuth: []
+ *              requestBody:
+ *                  required: false
+ *              responses:
+ *                  "200":
+ *                      description: User exists, data is added to user
+ *                  "403":
+ *                      description: Token could not be verified
+ */
 router.put('/setData', verifyToken, (req, res) => {
     jwt.verify(req.token, tokenSecret, (err, authData) => {
         if(err) res.sendStatus(403);
